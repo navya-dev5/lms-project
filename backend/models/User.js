15c53e4dc051b48520f69ebb4b5f1804f5ced6e1
navya-dev5 <potunavya5@gmail.com>
2026-03-13 16:32:44 +0530

    commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, trim: true },
+    name: { type: String, required: true, trim: true           },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true, minlength: 6, select: false },
     role: {
